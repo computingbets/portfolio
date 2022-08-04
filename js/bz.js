69865c4ea561f8059c6e0e45611a387e23879b80
MisterScript <staley77@protonmail.com>
2022-08-04 14:14:19 -0400

    format js files. first prettier run

--- js/bz.js
+++ js/bz.js
@@ -3,13 +3,12 @@ scrollTop() >= 300
 Should be equal the the height of the header
 ========================================== */
 
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 275) {
-        $('nav').addClass('fixed-header');
-        $('nav div').addClass('visible-title');
-    }
-    else {
-        $('nav').removeClass('fixed-header');
-        $('nav div').removeClass('visible-title');
-    }
-});
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 275) {
+    $('nav').addClass('fixed-header')
+    $('nav div').addClass('visible-title')
+  } else {
+    $('nav').removeClass('fixed-header')
+    $('nav div').removeClass('visible-title')
+  }
+})
